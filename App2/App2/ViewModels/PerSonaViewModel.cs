a66e95ea6adfe8aeca617f24480332472b3fd1cf
Edgar Mollo Flores <molloemf@gmail.com>
2018-06-26 18:50:03 -0400

    Se creo y se implemento la clase PerSonaViewModel.cs

---
+++ App2/App2/ViewModels/PerSonaViewModel.cs
@@ -0,0 +1,76 @@
+﻿using App2.Models;
+using App2.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2.ViewModels
+{
+    public class PersonaViewModel:PersonaModel
+    {
+        public ObservableCollection<PersonaModel> Personas { get; set; }
+        PersonaServicio servicio = new PersonaServicio();
+        PersonaModel modelo;
+        public PersonaViewModel()
+        {
+            Personas = servicio.Consultar();
+            GuardarCommand = new Command(async () => await Guardar(), () => !Isbusy);
+            ModificarCommand = new Command(async () => await Modificar(), () => !Isbusy);
+            EliminarCommand = new Command(async () => await Eliminar(), () => !Isbusy);
+            LimpiarCommand = new Command(Limpiar(),() => !Isbusy);
+        }
+
+        public Command GuardarCommand { get; set; }
+        public Command ModificarCommand { get; set; }
+        public Command EliminarCommand { get; set; }
+        public Command LimpiarCommand { get; set; }
+
+        private async Task Guardar()
+        {
+            Isbusy = true;
+            Guid IdPersona = Guid.NewGuid();
+            modelo = new PersonaModel()
+            {
+                Nombre = Nombre,
+                Apellido = Apellido,
+                Edad = Edad,
+                Id = IdPersona.ToString()
+            };
+            servicio.Guardar(modelo);
+            await Task.Delay(2000);
+            Isbusy=false;
+        }
+        private async Task Modificar()
+        {
+            Isbusy = true;
+
+            modelo = new PersonaModel()
+            {
+                Nombre = Nombre,
+                Apellido = Apellido,
+                Edad = Edad,
+                Id = Id
+            };
+            servicio.Modificar(modelo);
+            await Task.Delay(2000);
+            Isbusy = false;
+        }
+        private async Task Eliminar()
+        {
+            Isbusy = true;
+            servicio.Eliminar(Id);
+            await Task.Delay(2000);
+            Isbusy = false;
+        }
+        private void Limpiar()
+        {
+            Nombre = "";
+            Apellido = "";
+            Edad = 0;
+            Id = "";
+        }
+}
+}
