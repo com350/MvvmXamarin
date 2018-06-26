31c1363e2e94df5da57de22b1ed67ce2ab0c9425
= <=>
2018-06-26 19:06:28 -0400

    Primera vista Persona

---
+++ App2/App2/Views/PersonaPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PersonaPage : ContentPage
+	{
+        PersonaViewModel contexto = PersonaViewModel();
+		public PersonaPage ()
+		{
+			InitializeComponent ();
+            BindingContext = contexto;
+            LvPersonas.ItemSelected += LvPersonas_ItemSelected;
+
+		}
+
+        private void LvPersonas_ItemSelected( object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem!=null)
+            {
+                PersonaModel modelo = (PersonaModel)e.SelectedItem;
+
+                Navigation.PushAsync(new DetallePage(modelo));
+                //contexto.Nombre = modelo.Nombre;
+                //contexto.Apellido = modelo.Apellido;
+                //contexto.Edad = modelo.Edad;
+                //contexto.Id = modelo.id;
+
+            }
+        }
+	}
+}
