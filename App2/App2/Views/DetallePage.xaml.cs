d81ec1031ede0fc194184954c652d30139145c5c
= <=>
2018-06-26 19:10:30 -0400

    Segunda plantilla Persoan

---
+++ App2/App2/Views/DetallePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using App2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetallePage : ContentPage
+	{
+		public DetallePage ( PersonaModel modelo)
+		{
+			InitializeComponent ();
+            BindingContext = modelo;
+		}
+	}
+}
