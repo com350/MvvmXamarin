ef7c9d34a3fd95350fb2f5291308e5b14eb6a5fa
= <=>
2018-06-26 19:29:45 -0500

    Implementacion de la clase PersonaModel

---
+++ App2/App2/Models/PersonaModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace App2.Models
+{
+    public class PersonaModel: System.ComponentModel.INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange([CallerMemberName]string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        private bool isBusy = false;
+
+        public bool Isbusy
+        {
+            get { return Isbusy=false; }
+            set { isBusy = value;
+                OnPropertyChange();
+            }
+        }
+
+
+
+        private string id;
+        public string  Id {
+            get { return id; }
+            set { id = value;
+                OnPropertyChange();
+            }
+        }
+
+        private string nombre;
+        public string Nombre {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChange();
+                OnPropertyChange(nameof(NombreCompleto));
+            }
+        }
+
+        private string apellido;
+        public string  Apellido {
+            get { return apellido; }
+            set {apellido=value;
+
+                OnPropertyChange();
+                OnPropertyChange(nameof(NombreCompleto));
+            }
+        }
+
+        private int edad; 
+        public int Edad {
+            get { return edad; }
+            set {edad=value;
+
+                OnPropertyChange();
+                
+            }
+        }
+        private string nombreCompleto;
+        public string NombreCompleto {
+            get { return $"{Nombre}{Apellido}";
+            } set { nombreCompleto = value;
+
+                OnPropertyChange();
+                OnPropertyChange(nameof(Mesaje));
+            }
+        }
+        private string mensaje;
+        public string Mesaje {
+            get { return $"Tu Nombre es {NombreCompleto}"; }
+                 
+        }
+
+        
+    }
+
+}
